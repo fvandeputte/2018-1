af822c8411b3d950a9095dd4a9720517ddc2eeb0
Vicente Errázuriz <verrazuriz@uc.cl>
2018-03-08 22:07:57 -0300

    Actividad 0

---
+++ Tareas/Actividad 0/Programa/src/listas/linkedlist/linkedlist.c
@@ -0,0 +1,55 @@
+// importo el archivo linkedlist.h
+#include "linkedlist.h"
+// Libreria estandar de C
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////////////////
+//                             Funciones                                //
+//////////////////////////////////////////////////////////////////////////
+
+// TODO: debes completar las funciones que estan a continuacion
+// Puedes crear otras funciones aca para el
+// funcionamiento interno del arreglo dinamico
+
+/** Crea una lista inicialmente vacia y retorna el puntero */
+LinkedList* linkedlist_init()
+{
+
+}
+
+
+/** Inserta un elemento al final de la lista */
+void linkedlist_append(LinkedList* list, int element)
+{
+
+}
+
+/** Inserta el elemento dado en la posicion indicada */
+void linkedlist_insert(LinkedList* list, int element, int position)
+{
+
+}
+
+/** Elimina el elemento de la posicion indicada y lo retorna */
+int linkedlist_delete(LinkedList* list, int position)
+{
+
+}
+
+/** Retorna el valor del elemento en la posicion dada */
+int linkedlist_get(LinkedList* list, int position)
+{
+
+}
+
+/** Concatena a la lista una segunda lista */
+void linkedlist_concatenate(LinkedList* list, LinkedList* list2)
+{
+
+}
+
+/** Libera todos los recursos asociados a la lista */
+void linkedlist_destroy(LinkedList* list)
+{
+
+}
---
+++ Tareas/Actividad 0/Programa/src/listas/main.c
@@ -0,0 +1,13 @@
+// Libreria de input y output (para leer y escribir archivos o leer y escribir en consola)
+#include <stdio.h>
+
+// Importo los archivos .h de las listas
+#include "arraylist/arraylist.h"
+#include "linkedlist/linkedlist.h"
+
+// Recibe como input los parametros del programa en un arreglo de strings y un numero que indica cuantos argumentos son
+int main(int argc, char *argv[])
+{
+  // Retorna 0 cuando la ejecucion del programa es correcta
+  return 0;
+}
